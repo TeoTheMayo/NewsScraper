8f6d560dcd404f4248eef55c5afda174dcd89900
TeoTheMayo121 <teothemayo121@gmail.com>
2025-02-20 20:06:42 -0600

    Optimized code

--- newscraper.py
+++ newscraper.py
@@ -8,7 +8,7 @@ import configparser
 
 #Initialze the config
 config = configparser.ConfigParser()
-config.read("conf.conf")
+config.read("/Users/matteo/Desktop/SeriousProjects/NewScraper/conf.conf")
 
 
 target_url = "https://apnews.com/"
@@ -26,20 +26,6 @@ headers = {
 r = requests.get(target_url, headers=headers)#fetch site
 
 soup = BeautifulSoup(r.text, "html.parser")
-
-#function for getting main story on AP news,
-#Needs to be improved
-def findHeadline(soup):
-    firstElement = soup.find(class_ = "PageListStandardE-items")
-    list = []
-    if firstElement:
-        child = firstElement.find(class_ = "PagePromo-title")
-        if child:
-            grandson = child.find("a")
-            if grandson:
-                list.append(grandson.get_text())
-                list.append(grandson.get("href"))
-    return list
 
 #function for finding other relevant headlines
 def findBlocks(soup):
@@ -59,22 +45,25 @@ def findBlocks(soup):
 
 #function for extracting the information from the list and making it neat through html
 #Plan: Maybe fetch images as well?
-def convertingListHtml(list, firstHeadlineList):
+def convertingListHtml(soup, list):
     
-    html_body = "<h1>News Articles of the Day</h1>"
-    html_body += f'<h3>Main Story: {firstHeadlineList[0]}, <a href = "{firstHeadlineList[1]}">Read More</a></h3>'
+    #Gets the main headline
+    firstElement = soup.find(class_ = "PagePromo-title")
+    link = firstElement.find("a")
+    link = link.get("href")
 
-        
+    html_body = "<h1>News Articles of the Day</h1>"
+    html_body += f'<h3>Main Story: {firstElement}, <a href = "{link}">Read More</a></h3>' 
     html_body += "SECONDARY STORIES:"
     
     #Only writing out first 11 entries considering many of the stories at the bottom of the page are irrelevent/ads
     #Plan: WRITE ALGO TO ADD WEIGHTS TO KEY WORDS (TRUMP, UKRAINE, CHINA, FATAL, ETC) AND FETCH MOST RELEVANT STORIES BASED ON WEIGHT
     for i in range(0, 4):
-        html_body += f'<p>{list[i][0]}<a href = "{list[i][1]}">Read More<a/></p>'
+        html_body += f'<p>{list[i][0]}<a href = "{list[i][1]}"> Read More<a/></p>'
 
     html_body += "<h5>OTHER RELEVANT STORIES</h5>"
     for i in range(4, 10):
-        html_body += f'<p>{list[i][0]}<a href = "{list[i][1]}">Read More<a/></p>'
+        html_body += f'<p>{list[i][0]}<a href = "{list[i][1]}"> Read More<a/></p>'
 
     return html_body
 
@@ -84,9 +73,10 @@ msg = MIMEMultipart()
 msg["Subject"] = "Today's News"
 me = config["DEFAULT"]["SENDER_EMAIL"]
 family = config["DEFAULT"]["RECEIVER_EMAIL"]
+george = config["DEFAULT"]["GEORGE_RECEIVER_EMAIL"]
 msg["From"] = me
 msg["To"] = me
-html_body = convertingListHtml(findBlocks(soup), findHeadline(soup))
+html_body = convertingListHtml(soup, findBlocks(soup))
 msg.attach(MIMEText(html_body, "html"))
 
 SMTP_SERVER = "smtp.gmail.com"
